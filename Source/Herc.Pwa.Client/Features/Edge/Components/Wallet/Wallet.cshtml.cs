aadb93541106d3da91b5fb398eac0bee7ef81f91
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2018-10-30 01:34:19 +0000

    Add Bootstrap places holder etc... Way to big of a commit.  Feature of being the only dev.

---
+++ Source/Herc.Pwa.Client/Features/Edge/Components/Wallet/Wallet.cshtml.cs
@@ -0,0 +1,10 @@
+﻿namespace Herc.Pwa.Client.Features.Edge.Components.Wallet
+{
+  using Herc.Pwa.Client.Components;
+
+  public class WalletModel : BaseComponent
+  {
+    public string Balance { get; set; } = "500";
+    public string CurrencyCode { get; set; } = "HERC";
+  }
+}
---
+++ Source/Herc.Pwa.Client/Features/Edge/ShowLoginWindow/ShowLoginWindowEdgeActionHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Herc.Pwa.Client.Features.Edge
+{
+  using System.Threading;
+  using System.Threading.Tasks;
+  using BlazorState;
+  using Herc.Pwa.Client.Features.Base;
+  using Microsoft.JSInterop;
+
+  //TODO: this doesn't use any state so maybe doesn't need BaseHandler
+  public partial class EdgeState
+  {
+    public class ShowLoginWindowEdgeActionHandler : BaseHandler<ShowLoginWindowEdgeAction, EdgeState>
+    {
+      public ShowLoginWindowEdgeActionHandler(IStore aStore) : base(aStore) { }
+
+      public override async Task<EdgeState> Handle(ShowLoginWindowEdgeAction showLoginWindowEdgeRequest, CancellationToken cancellationToken)
+      {
+        await JSRuntime.Current.InvokeAsync<bool>("EdgeInterop.ShowLoginWindow");
+        return EdgeState;
+      }
+    }
+  }
+}
