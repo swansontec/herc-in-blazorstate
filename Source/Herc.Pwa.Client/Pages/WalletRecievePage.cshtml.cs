4b9c56ee622d220fda807322799a5bec29182788
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2018-11-21 08:34:49 +0000

    Clean up Wallet Pages.

--- Source/Herc.Pwa.Client/Pages/WalletRecievePage.cshtml.cs
+++ Source/Herc.Pwa.Client/Pages/WalletRecievePage.cshtml.cs
@@ -1,33 +1,27 @@
 ﻿namespace Herc.Pwa.Client.Pages
 {
-  using System;
-  using System.Linq;
   using System.Net;
   using System.Threading.Tasks;
   using Herc.Pwa.Client.Components;
   using Herc.Pwa.Client.Features.Clipboard;
   using Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet;
-  using Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet.Send;
   using Herc.Pwa.Client.Services;
-  using Herc.Pwa.Shared;
-  using Herc.Pwa.Shared.Enumerations.FeeOption;
   using Microsoft.AspNetCore.Blazor.Components;
   using Microsoft.JSInterop;
 
   public class WalletReceivePageModel : BaseComponent
   {
-    [Inject] public AmountConverter AmountConverter { get; set; }
-    public static string Route(string aEdgeCurrencyWalletEncodedId) => $"/wallet/{aEdgeCurrencyWalletEncodedId}/Receive";
-
-    [Parameter] protected string EdgeCurrencyWalletEncodedId { get; set; }
-
-    private string EdgeCurrencyWalletId => WebUtility.UrlDecode(EdgeCurrencyWalletEncodedId);
     private EdgeCurrencyWallet EdgeCurrencyWallet => EdgeCurrencyWalletsState.EdgeCurrencyWallets[EdgeCurrencyWalletId];
-
+    private string EdgeCurrencyWalletId => WebUtility.UrlDecode(EdgeCurrencyWalletEncodedId);
+    [Parameter] protected string EdgeCurrencyWalletEncodedId { get; set; }
+    [Inject] public AmountConverter AmountConverter { get; set; }
     public string ReceiveAddress => EdgeCurrencyWallet.Keys["ethereumAddress"];
 
-    protected async Task CopyToClipboardAsync() => 
+    public string WalletName => EdgeCurrencyWallet.Name;
+
+    protected async Task CopyToClipboardAsync() =>
       await JSRuntime.Current.InvokeAsync<object>(ClipboardInteropMethodNames.ClipboardInterop_WriteText, ReceiveAddress);
 
+    public static string Route(string aEdgeCurrencyWalletEncodedId) => $"/wallet/{aEdgeCurrencyWalletEncodedId}/Receive";
   }
 }
