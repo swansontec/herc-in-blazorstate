be022f6a2feec922f96b3ba852b2e8a5dae5c411
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2018-11-21 06:26:07 +0000

    Update WalletRecievePage with QRCode and CopyToClipboard

---
+++ Source/Herc.Pwa.Js/source/Clipboard/ClipboardInterop.ts
@@ -0,0 +1,18 @@
+﻿export const ClipboardInteropName: string = 'ClipboardInterop';
+
+export class ClipboardInterop {
+
+  constructor() {
+    console.log('ClipboardInterop constructor');
+  }
+
+  public WriteText = async (text: string): Promise<boolean> => {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    return true;
+  }
+}
