7e55144dbdd6318f1223204b5d4a680f915486dc
stackbaxter <stackbaxter@yahoo.com>
2018-11-29 18:26:17 -0600

    changePass page model and link

---
+++ Source/Herc.Pwa.Client/Pages/ChangePassword.cshtml.cs
@@ -0,0 +1,11 @@
+namespace Herc.Pwa.Client.Pages
+{
+  using Herc.Pwa.Client.Components;
+    public class ChangePasswordModel : BaseComponent
+    {
+  public const string Route = "/changePassword";
+  //public void OnGet()
+  //{
+  //}
+}
+}
