4398c8bba4d7b790725ff493b0e74b7270f9e793
stackbaxter <stackbaxter@yahoo.com>
2018-11-29 20:37:08 -0600

    change

--- Source/Herc.Pwa.Client/Pages/ChangePassword.cshtml.cs
+++ Source/Herc.Pwa.Client/Pages/ChangePassword.cshtml.cs
@@ -1,11 +1,26 @@
 namespace Herc.Pwa.Client.Pages
 {
   using Herc.Pwa.Client.Components;
-    public class ChangePasswordModel : BaseComponent
-    {
-  public const string Route = "/changePassword";
-  //public void OnGet()
-  //{
-  //}
-}
-}
+  public class ChangePasswordModel : BaseComponent
+  {
+    public const string Route = "/changePassword";
+    public string NewPassword { get; set; }
+
+  }
+} 
+// The change PW function from the edge docs, need to change "abcAccount" to whatever the context is called
+// I think this is the right place to put it but am not sure...    
+  
+  //    await abcAccount.changePassword(password)
+//- (void) changePassword:(NSString*) password
+//              callback:(void (^)(AbcError* error)) callback;
+
+//// Example
+//[abcAccount changePassword:password callback:^(AbcError* error) {
+//    if (error) {
+//        // Oh no
+//    } else {
+//        // Yay, new password set
+//    }
+//}];
+
