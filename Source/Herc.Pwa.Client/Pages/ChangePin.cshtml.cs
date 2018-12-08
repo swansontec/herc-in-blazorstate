4582c1f36ea46bdf0e1458f87b41dc10b09a1e5c
stackbaxter <stackbaxter@yahoo.com>
2018-12-08 00:34:26 -0600

    ChangePin UI started

---
+++ Source/Herc.Pwa.Client/Pages/ChangePin.cshtml.cs
@@ -0,0 +1,21 @@
+namespace Herc.Pwa.Client.Pages
+{
+  using System;
+  using System.Threading.Tasks;
+  using FluentValidation.Results;
+  using Herc.Pwa.Client.Components;
+  using Herc.Pwa.Client.Features.Edge.EdgeAccount.
+
+
+  public class ChangePinModel : BaseComponent
+  {
+    public const string Route = "/changePassword";
+    public string NewPassword { get; set; }
+    public string ConfirmPassword { get; set; }
+    public ValidationResult ValidationResult { get; set; }
+
+    public void OnGet()
+    {
+    }
+  }
+}
