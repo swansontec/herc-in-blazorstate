6ae36e1b2b5b166804ddf85cd5f25589107d3da9
stackbaxter <stackbaxter@yahoo.com>
2018-12-30 16:26:58 -0600

    style edits, getting closer

--- Source/Herc.Pwa.Client/Features/Edge/Components/EdgeTransactionComponent.cshtml.cs
+++ Source/Herc.Pwa.Client/Features/Edge/Components/EdgeTransactionComponent.cshtml.cs
@@ -3,13 +3,31 @@ namespace Herc.Pwa.Client.Features.Edge.Components
   using Herc.Pwa.Client.Components;
   using Herc.Pwa.Client.Features.Edge.State;
   using Microsoft.AspNetCore.Blazor.Components;
+  using Herc.Pwa.Client.Services;
 
   public class EdgeTransactionComponentModel : BaseComponent
   {
 
     [Parameter] protected EdgeTransaction EdgeTransaction { get; set; }
-
     public string SendOrReceive(string aTransValueAmount) => aTransValueAmount.Substring(0,1) == "-" ? "Sent" : "Received";
+
+    [Inject] public AmountConverter AmountConverter { get; set; }
+    private int Granularity => 18; // multiplier is 10^18 18 places 
+    [Parameter] protected int DecimalPlacesToDisplay { get; set; } = 6;
+    public string DisplayBalance
+    {
+      get
+      {
+        var balanceFormaterRequest = new FormatAmountRequest()
+        {
+          Amount = EdgeTransaction.NativeAmount,
+          Granularity = Granularity,
+          DecimalPlacesToDisplay = DecimalPlacesToDisplay,
+        };
+        return AmountConverter.GetFormatedAmount(balanceFormaterRequest);
+      }
+    }
 
   }
 }
+
