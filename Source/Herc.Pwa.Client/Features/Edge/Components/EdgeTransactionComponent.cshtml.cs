650ed885b01f4e8413ed35fff0210a87950965a9
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2019-01-09 05:16:26 +0000

    Switch to Bootstrap Table.

--- Source/Herc.Pwa.Client/Features/Edge/Components/EdgeTransactionComponent.cshtml.cs
+++ Source/Herc.Pwa.Client/Features/Edge/Components/EdgeTransactionComponent.cshtml.cs
@@ -1,21 +1,23 @@
 namespace Herc.Pwa.Client.Features.Edge.Components
 {
+  using System.Globalization;
   using Herc.Pwa.Client.Components;
   using Herc.Pwa.Client.Features.Edge.State;
-  using Microsoft.AspNetCore.Blazor.Components;
   using Herc.Pwa.Client.Services;
+  using Microsoft.AspNetCore.Blazor.Components;
 
   public class EdgeTransactionComponentModel : BaseComponent
   {
 
     [Parameter] protected EdgeTransaction EdgeTransaction { get; set; }
-    [Parameter] protected int DecimalPlacesToDisplay { get; set; } = 8;
-    public string SendOrReceive(string aTransValueAmount) => aTransValueAmount.Substring(0,1) == "-" ? "Sent" : "Received";
+    [Parameter] protected int DecimalPlacesToDisplay { get; set; } = 6;
+    public string SendOrReceive(string aTransValueAmount) => aTransValueAmount.Substring(0, 1) == "-" ? "Sent" : "Received";
     private int Granularity { get; set; } = 18; // multiplier is 10^18 18 places 
 
+    public string IsoDateString => EdgeTransaction.Date.ToString("yyyy-MM-dd' T'HH:mm:ss", CultureInfo.InvariantCulture);
+    public string EthScanUrl => $"https://etherscan.io/tx/TODO";
     [Inject] public AmountConverter AmountConverter { get; set; }
-    public string DisplayBalance
-
+    public string TransactionAmount
     {
       get
       {
