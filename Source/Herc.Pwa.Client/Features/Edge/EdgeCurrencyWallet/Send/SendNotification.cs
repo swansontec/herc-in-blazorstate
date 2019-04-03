d5ec952b8785be761ee567cbd73fe17484db8567
Steven T. Cramer <SteveTCramer@gmail.com>
2019-04-03 06:38:37 +0000

    Update E2E Tests to netcoreapp3.0
    Add some serialization tests that help us find and fix edge bug.

---
+++ Source/Herc.Pwa.Client/Features/Edge/EdgeCurrencyWallet/Send/SendNotification.cs
@@ -0,0 +1,10 @@
+﻿namespace Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet
+{
+  using MediatR;
+
+  internal class SendNotification : INotification
+  {
+    public SendAction SendAction { get; set; }
+    public string TransactionId { get; set; }
+  }
+}
