d9c46d0be59fead770e9765c794feee847bc575a
Steven T. Cramer <SteveTCramer@gmail.com>
2019-04-07 05:52:31 +0000

    Add SendValidator and associated Test cases.

---
+++ Test/Herc.Pwa.Client.Integration.Tests/Features/Edge/EdgeCurrencyWallet/Send/SendTests.cs
@@ -0,0 +1,248 @@
+﻿namespace Herc.Pwa.Client.Integration.Tests.Features.Edge.EdgeAccount.ChangePassword
+{
+  using System;
+  using System.Linq;
+  using BlazorState;
+  using BlazorState.Integration.Tests.Infrastructure;
+  using FluentValidation;
+  using FluentValidation.Results;
+  using FluentValidation.Validators;
+  using Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet;
+  using Herc.Pwa.Shared.Enumerations.FeeOption;
+  using MediatR;
+  using Microsoft.Extensions.DependencyInjection;
+  using Shouldly;
+
+  internal class SendTests
+  {
+    private const string WalletId = "1fu3+YTRMVRb6R6uwO7DDCH31iVKkBMtkYHLA0P3hMo=";
+
+    public SendTests(TestFixture aTestFixture)
+    {
+      ServiceProvider = aTestFixture.ServiceProvider;
+      Mediator = ServiceProvider.GetService<IMediator>();
+      Store = ServiceProvider.GetService<IStore>();
+      EdgeCurrencyWalletsState = Store.GetState<EdgeCurrencyWalletsState>();
+    }
+
+    private EdgeCurrencyWalletsState EdgeCurrencyWalletsState { get; set; }
+    private IMediator Mediator { get; }
+    private IServiceProvider ServiceProvider { get; }
+    private IStore Store { get; }
+    private EdgeCurrencyWalletsState ValidEdgeCurrencyWalletsState { get; set; }
+    private SendAction SendAction { get; set; }
+
+    private const string HercStartBalance = "1500000000000000000";
+
+    public void Setup()
+    {
+      // Setup known state valid State.required for a Send to be valid
+
+      ValidEdgeCurrencyWalletsState = new EdgeCurrencyWalletsState();
+      // setup wallet
+
+      var edgeCurrencyWallet = new EdgeCurrencyWallet
+      {
+        SelectedCurrencyCode = "HERC"
+      };
+      edgeCurrencyWallet.Balances.Add("HERC", HercStartBalance); //1.5 HERC
+
+      ValidEdgeCurrencyWalletsState.EdgeCurrencyWallets.Add(
+        WalletId,
+        edgeCurrencyWallet);
+      EdgeCurrencyWalletsState.Initialize(ValidEdgeCurrencyWalletsState);
+
+      SendAction = new SendAction
+      {
+        CurrencyCode = "HERC",
+        DestinationAddress = "0xe3d6f1e0434b870c3b3a0066bdcbffd4ba3f7ea6",
+        EdgeCurrencyWalletId = "1fu3+YTRMVRb6R6uwO7DDCH31iVKkBMtkYHLA0P3hMo=",
+        NativeAmount = "500000000000000000", // 0.5 HERC
+        Fee = FeeOption.Standard,
+      };
+
+    }
+
+    public void CurrencyCodeIsRequired()
+    {
+      // Arrange
+
+      SendAction.CurrencyCode = "";
+
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+
+      ValidationFailure validationFailure = validationResult.Errors[0];
+      validationFailure.PropertyName.ShouldBe(nameof(SendAction.CurrencyCode));
+      validationFailure.ErrorCode.ShouldBe(nameof(NotEmptyValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+
+    public void CurrencyCodeMustBeValid()
+    {
+      // Arrange
+
+      SendAction.CurrencyCode = "BAD";
+
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+      ValidationFailure validationFailure = validationResult.Errors[0];
+
+      validationFailure.PropertyName.ShouldBe(string.Empty);
+      validationFailure.ErrorCode.ShouldBe(nameof(PredicateValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+
+    public void DestinationAddressIsRequired()
+    {
+      // Arrange
+
+      SendAction.DestinationAddress = "";
+      
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+      ValidationFailure validationFailure = validationResult.Errors[0];
+
+      validationFailure.PropertyName.ShouldBe(nameof(SendAction.DestinationAddress));
+      validationFailure.ErrorCode.ShouldBe(nameof(NotEmptyValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+
+    public void DestinationAddressMustBeValid()
+    {
+      // Arrange
+
+      SendAction.DestinationAddress = "ThisIsNotValid";
+
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+      ValidationFailure validationFailure = validationResult.Errors[0];
+
+      validationFailure.PropertyName.ShouldBe(nameof(SendAction.DestinationAddress));
+      validationFailure.ErrorCode.ShouldBe(nameof(PredicateValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+
+    public void FeeIsRequired()
+    {
+      // Arrange
+      SendAction.Fee = null;
+
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+      ValidationFailure validationFailure = validationResult.Errors[0];
+
+      validationFailure.PropertyName.ShouldBe(nameof(SendAction.Fee));
+      validationFailure.ErrorCode.ShouldBe(nameof(NotEmptyValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+
+    public void NativeAmountMustBeGreaterThanZero()
+    {
+      // Arrange
+      SendAction.NativeAmount = "0000";
+      
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+      ValidationFailure validationFailure = validationResult.Errors[0];
+
+      validationFailure.PropertyName.ShouldBe(nameof(SendAction.NativeAmount));
+      validationFailure.ErrorCode.ShouldBe(nameof(PredicateValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+
+    public void NativeAmountMustBeLessThanBalance()
+    {
+      // Arrange
+      SendAction.NativeAmount = $"1{HercStartBalance}"; // Greater than Balance
+      
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+
+      ValidationFailure validationFailure = validationResult.Errors[0];
+      validationFailure.PropertyName.ShouldBe(string.Empty);
+      validationFailure.ErrorCode.ShouldBe(nameof(PredicateValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+
+    public void ShouldBeValid()
+    {
+      // Arrange
+
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(true);
+    }
+
+    public void WalletDoesNotExist()
+    {
+      SendAction.EdgeCurrencyWalletId = "x";
+        
+      var sendValidator = new SendValidator(Store);
+
+      // Act
+
+      ValidationResult validationResult = sendValidator.Validate(SendAction);
+
+      // Assert
+      validationResult.IsValid.ShouldBe(false);
+      validationResult.Errors.Count.ShouldBe(1);
+
+      ValidationFailure validationFailure = validationResult.Errors[0];
+      validationFailure.PropertyName.ShouldBe(nameof(SendAction.EdgeCurrencyWalletId));
+      validationFailure.ErrorCode.ShouldBe(nameof(PredicateValidator));
+      validationFailure.Severity.ShouldBe(Severity.Error);
+    }
+  }
+}
