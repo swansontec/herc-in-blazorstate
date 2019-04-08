4cf9366e0edd1af4aa2d92a545f5420346fb89ce
Steven T. Cramer <SteveTCramer@gmail.com>
2019-04-08 15:19:50 +0000

    Initialize all strings in SendActions to Empty.
    Prep for C# 8 nullable reference types.

--- Source/Herc.Pwa.Client/Features/Edge/EdgeCurrencyWallet/Send/SendAction.cs
+++ Source/Herc.Pwa.Client/Features/Edge/EdgeCurrencyWallet/Send/SendAction.cs
@@ -6,16 +6,16 @@
 
   public class SendAction : BaseRequest, IRequest<EdgeCurrencyWalletsState>
   {
-    public string EdgeCurrencyWalletId { get; set; }
-    public string DestinationAddress { get; set; }
+    public string EdgeCurrencyWalletId { get; set; } = string.Empty;
+    public string DestinationAddress { get; set; } = string.Empty;
 
     /// <summary>
     /// The amount without decimals. For ETH the native amount unit is called wei. 1x10^18 wei = 1 ETH
     /// </summary>
-    public string NativeAmount { get; set; }
+    public string NativeAmount { get; set; } = string.Empty;
 
-    public string CurrencyCode { get; set; }
+    public string CurrencyCode { get; set; } = string.Empty;
 
-    public FeeOption Fee { get; set; }
+    public FeeOption Fee { get; set; } = FeeOption.Standard;
   }
 }
