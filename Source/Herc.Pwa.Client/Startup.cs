658f1397421998911dd347438dfa4e308af4fa27
stackbaxter <stackbaxter@yahoo.com>
2019-04-25 17:35:58 -0500

    builds through client on down

--- Source/Herc.Pwa.Client/Startup.cs
+++ Source/Herc.Pwa.Client/Startup.cs
@@ -2,30 +2,33 @@
 {
   //using Blazor.Extensions.Logging;
   using BlazorState;
+  using BlazorState.Services;
   using FluentValidation;
   using Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet;
   using Herc.Pwa.Client.Services;
   using Herc.Pwa.Client.Shared;
   using Microsoft.AspNetCore.Components.Builder;
   using Microsoft.Extensions.DependencyInjection;
-  using Microsoft.Extensions.Logging;
   using Nethereum.Util;
 
   public class Startup
   {
-    public void Configure(IComponentsApplicationBuilder aBlazorApplicationBuilder) =>
-      aBlazorApplicationBuilder.AddComponent<App>("app");
+    public void Configure(IComponentsApplicationBuilder aComponentsApplicationBuilder) =>
+      aComponentsApplicationBuilder.AddComponent<App>("app");
 
     public void ConfigureServices(IServiceCollection aServiceCollection)
     {
-      aServiceCollection.AddSingleton<ColorPalette>();
-      aServiceCollection.AddSingleton<AmountConverter>();
-      aServiceCollection.AddSingleton<AddressUtil>();
-      aServiceCollection.AddScoped(typeof(IValidator<SendAction>), typeof(SendValidator));
-      //aServiceCollection.AddLogging(aLoggingBuilder => aLoggingBuilder
-      //    .AddBrowserConsole()
-      //    .SetMinimumLevel(LogLevel.Trace)
-      //);
+      if (new BlazorHostingLocation().IsClientSide)
+      {
+	      aServiceCollection.AddSingleton<ColorPalette>();
+	      aServiceCollection.AddSingleton<AmountConverter>();
+	      aServiceCollection.AddSingleton<AddressUtil>();
+	      aServiceCollection.AddScoped(typeof(IValidator<SendAction>), typeof(SendValidator));
+        // TODO add this back once Blazor.Extentions.Logging is updated to 0.8.0
+        //aServiceCollection.AddLogging(aLoggingBuilder => aLoggingBuilder
+        //    .AddBrowserConsole()
+        //    .SetMinimumLevel(LogLevel.Trace));
+      };
       aServiceCollection.AddBlazorState();
     }
   }
