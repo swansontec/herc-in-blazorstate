3f95726520576cbaa847059302b55e9ad02ee91a
stackbaxter <stackbaxter@yahoo.com>
2019-04-25 03:28:05 -0500

    renaming razor pages

--- Source/Herc.Pwa.Client/App.razor.cs
+++ Source/Herc.Pwa.Client/App.razor.cs
@@ -1,9 +1,9 @@
 ﻿namespace Herc.Pwa.Client
 {
   using System.Threading.Tasks;
+  using BlazorState.Pipeline.ReduxDevTools;
   using BlazorState.Features.JavaScriptInterop;
   using BlazorState.Features.Routing;
-  using BlazorState.Pipeline.ReduxDevTools;
   using Microsoft.AspNetCore.Components;
 
   public class AppModel : ComponentBase
@@ -11,12 +11,14 @@
     // Injected so they are created by the container even though ide says not used.
     [Inject] private JsonRequestHandler JsonRequestHandler { get; set; }
     [Inject] private ReduxDevToolsInterop ReduxDevToolsInterop { get; set; }
+
+    // Injected so it is created by the container even though ide says not used it is.
     [Inject] private RouteManager RouteManager { get; set; }
 
-    protected override async Task OnInitAsync()
+    protected override async Task OnAfterRenderAsync()
     {
-      await JsonRequestHandler.InitAsync();
       await ReduxDevToolsInterop.InitAsync();
+      await JsonRequestHandler.InitAsync();
     }
   }
 }
